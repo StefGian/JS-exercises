9157a0adeef75b708008f48e40c7e33b6cdcf063
Stefania Giannopoulou <stefania.gian91@gmail.com>
2019-10-17 11:47:58 +0300

    jQuery simple select exercises

---
+++ jQuery Select Exercises/script.js
@@ -0,0 +1,8 @@
+//select all divs and give them a purple background
+$("div").css("background", "purple");
+//select divs with class highlight and make them 200px wide
+$(".highlight").css("width", "200px");
+//select div with id third and give it an orange border
+$("#third").css("border", "orange 2px solid");
+//select the first div and give it font color pink
+$("div:first").css("color", "pink");
